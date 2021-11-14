c95b885ed54a5fd81de979130599cf7af22f19cd
charlie <2872237477@qq.com>
2021-11-14 17:01:31 +0800

    视频完结

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from "./store"
 
 import toast from "components/common/toast/index.js"
+import FastClick from "fastclick"
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -13,6 +15,13 @@ Vue.prototype.$bus = new Vue()
 // 安装toast插件
 Vue.use(toast)
 
+// 解决移动端300ms延迟
+FastClick.attach(document.body)
+
+// 使用懒加载插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/bgImage.jpg'),
+})
 new Vue({
   render: h => h(App),
   router,
